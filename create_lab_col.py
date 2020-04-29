93f67af9f42a4a22005217fbc589838903c8a586
Jakob Henning Jensen <jakobhenningjensen@hotmail.com>
2020-04-29 09:31:57 +0200

    Vectorized the creating of the list

--- create_lab_col.py
+++ create_lab_col.py
@@ -3,7 +3,6 @@ Script creating the LAB colors for all collage-images and storing them in "img_l
 
 """
 
-
 from PIL import Image #For Reading images
 import numpy
 import pickle
@@ -12,10 +11,7 @@ import numpy as np
 from skimage import color
 col_folder = "Resized_imgs/" #path to collage images
 imgs = os.listdir(col_folder) #All images
-n,m =(28,28) #Size of collage images
-img_list =np.zeros(shape=(len(imgs),n,m,3))
 
-for i,im in enumerate(imgs):
-    img_list[i]=color.rgb2lab(np.array(Image.open(col_folder+im)))
+img_list=[color.rgb2lab(np.array(Image.open(col_folder+im)) for im in imgs] #Convert each image to LAB values
 
 pickle.dump(img_list,open("img_list_lab","wb"))
